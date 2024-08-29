91423851511c2eedd77d02e2b257fb1437cf0ff3
“Gargi2275” <vanjaragargi@gmail.com>
2024-08-29 17:07:51 +0530

    footer and login

--- shelfshift_react/src/App.js
+++ shelfshift_react/src/App.js
@@ -1,5 +1,5 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Marquee from 'react-fast-marquee';
 import './App.css';
 import Navbar from './Components/Home/Navbar';
@@ -7,51 +7,36 @@ import ShuffleHero from './Components/Home/ShuffleHero';
 import MarqueeBg from './Components/Home/MarqueeBg';
 import { ParallaxHero } from './Components/Home/ParallaxHero';
 import Testimonial from './Components/Home/Testimonial';
+import Footer from './Components/Home/Footer';
+import Signin from './Components/Home/Signin';
+import Signup from './Components/Home/Signup';
 
 const App = () => {
-  // const [details, setDetails] = useState([]);
-
-  // useEffect(() => {
-  //   let data;
-  //   axios.get(`http://127.0.0.1:8000/`)
-  //     .then(res => {
-  //       data = res.data;
-  //       setDetails(data);
-  //     })
-  //     .catch(err=>{
-  //       console.log(err)
-  //     })
-  // }, []); // Empty dependency array to run only on mount
-
-  // return (
-  //   <div className='App bg-slate-600'>
-  //     <header>
-  //       Django Generated Data:
-  //     </header>
-  //     <hr />
-  //     {details.map((output, id) => (
-  //       <div key={id}>
-  //         <div>
-  //           <h2>
-  //             {output.product}
-  //           </h2>
-  //           <h3>
-  //             {output.price}
-  //           </h3>
-  //         </div>
-  //       </div>
-  //     ))}
-  //   </div>
-  // );
-
   return (
-    <>
-      <Navbar scroll='true'/>
-      <MarqueeBg/>
-      <ShuffleHero />
-      <ParallaxHero/>
-      <Testimonial/>
-    </>
+    <Router>
+      <Routes>
+        {/* Home Route */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar scroll='true' />
+              <MarqueeBg />
+              <ShuffleHero />
+              <ParallaxHero />
+              <Testimonial />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* Signin Route */}
+        <Route path="/signin" element={<Signin />} />
+
+        {/* Signup Route */}
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
