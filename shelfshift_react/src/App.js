518a924d13b2ddb1251355324d7d85e941d8f177
“Gargi2275” <vanjaragargi@gmail.com>
2024-09-14 22:29:11 +0530

    Gargi

--- shelfshift_react/src/App.js
+++ shelfshift_react/src/App.js
@@ -9,7 +9,14 @@ import Testimonial from './Components/Home/Testimonial';
 import Footer from './Components/Home/Footer';
 import Signin from './Components/Home/Signin';
 import Signup from './Components/Home/Signup';
-
+import Profile from './Components/Home/Profile';
+import Question from './Components/Home/Question';
+import FAQ from './Components/Home/FAQ';
+import Contact from './Components/Home/Contact';
+import Privacy_Policy from './Components/Home/Privacy_Policy';
+import Terms_Condition from './Components/Home/Terms_Condition';
+import Pricing from './Components/Pages/Pricing';
+import PricingCard from './Components/Pages/PricingCard'
 const App = () => {
   return (
     <>
@@ -37,19 +44,20 @@ const App = () => {
 
         {/* Signup Route */}
         <Route path="/signup" element={<Signup />} />
+        <Route path="/Question" element={<Question />} />
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/Privacy_Policy" element={<Privacy_Policy />} />
+        <Route path="/Terms_Condition" element={<Terms_Condition />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Pricing" element={<Pricing />} />
+
+
+
       </Routes>
     </Router>
   </>
   );
 }
 
-// const App = () => {
-//   return (
-//     <>
-//       {/* <Pricing/> */}
-//       {/* <DSidebar /> */}
-//     </>
-//   );
-// }
 
 export default App;
