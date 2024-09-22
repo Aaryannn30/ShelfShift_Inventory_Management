28af51aaaf1ba3160bf3e1be8bcdfa002a6f5144
Aaryannn30 <aryanmaru1230@gmail.com>
2024-09-23 04:14:44 +0530

    Aryan

--- Storify_frontend/src/Components/Dashboard/Item_form.js
+++ Storify_frontend/src/Components/Dashboard/Item_form.js
@@ -195,12 +195,13 @@ const ItemForm = () => {
           {/* Name, SKU, Unit, Returnable */}
           <div className="grid grid-cols-3 gap-6">
             <div>
-              <label className="block text-sm font-medium text-gray-700">Name</label>
+              <label className="block text-sm font-medium text-gray-700" >Name</label>
               <input
                 type="text"
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 placeholder="Enter Name"
                 value={name}
+                required
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
